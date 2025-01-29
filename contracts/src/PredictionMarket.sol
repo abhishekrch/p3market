0105aaad592b52d4d0d3c9fc17a01aa34cca931c
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-01-29 14:13:22 +0530

    Initial commit

---
+++ contracts/src/PredictionMarket.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@thirdweb-dev/contracts/eip/interface/IERC20.sol";
+import {Ownable} from "@thirdweb-dev/contracts/extension/Ownable.sol";
+import {ReentrancyGuard} from "@thirdweb-dev/contracts/external-deps/openzeppelin/security/ReentrancyGuard.sol";
+
+contract PredictionMarket is Ownable, ReentrancyGuard{
+    constructor() {}
+}
